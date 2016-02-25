5f718fe472164ea4ef2a8c1ebfdd3f9a0933b568
Francis DiTraglia <fditra@sas.upenn.edu>
2016-02-25 17:32:32 -0500

    update function to calculate observables so that it properly handles the case without covariates

--- R/binary.R
+++ R/binary.R
@@ -1,12 +1,36 @@
-# Function to calculate all the summary statistics of the data needed to use
-# our method for the identified set *without* sampling uncertainty
-getObs <- function(y_name, T_name, z_name, controls, data){
+#' Calculate summary statistics of observables
+#'
+#' @description This function calculates summary statistics for later use in the
+#' partial identification exercise for a binary, mis-measured, endogenous
+#' regressor based on a binary instrument that may itsef be endogenous.
+#' @param y_name Name of the outcome variable.
+#' @param T_name Name of the potentially endogenous and mis-measured binary
+#' treatment variable.
+#' @param z_name Name of the potentially endogenous binary instrument.
+#' @param controls Vector of names of exogenous control regressors.
+#' @param data Name of dataframe whose columns contain the data for the outcome,
+#' regressor, instrument, and controls.
+#'
+#' @return A list of summary statistics: probabilities, conditional means and
+#' variances, etc., in notation corresponding to the paper.
+#' @export
+#' @examples
+#' AngristControls <- c("age", "svy", "sex2", "phone", "hsvisit",
+#'                      "d1995", "djamundi", "dmonth1", "dmonth2",
+#'                      "dmonth3", "dmonth4", "dmonth5", "dmonth6", "dmonth7",
+#'                      "dmonth8", "dmonth9", "dmonth10", "dmonth11",
+#'                      "strata1", "strata2", "strata3", "strata4", "strata5")
+#' AngristY <- "totalRepeats"
+#' AngristT <- "usesch"
+#' AngristZ <- "vouch0"
+#' getObs(AngristY, AngristT, AngristZ, AngristControls, angrist)
+#'
+getObs <- function(y_name, T_name, z_name, controls = NULL, data){
   # Extract data for named columns
   y <- get(y_name, data)
   Tobs <- get(T_name, data)
   z <- get(z_name, data)
 
-  # Calculate all the observables
   obs <- list()
   obs$p <- mean(Tobs)
   obs$q <- mean(z)
@@ -29,26 +53,26 @@ getObs <- function(y_name, T_name, z_name, controls, data){
   obs$s2_10 <- var(y[Tobs == 1 & z == 0])
   obs$s2_11 <- var(y[Tobs == 1 & z == 1])
 
-  second_stage <- reformulate(c(T_name, controls), response = y_name)
-  first_stage <- reformulate(c(z_name, controls))
-  obs$gamma_iv <- coefficients(ivreg(second_stage, first_stage, data))[-c(1,2)]
-  # No intercept since we "project it out" by working with Cov matrix below
-  x <- model.matrix(reformulate(controls, intercept = FALSE), data)
-  obs$D_ETobs <- colMeans(x[Tobs == 1,]) - colMeans(x[Tobs == 0,])
-  invert_me <- rbind(cbind(cov(z, Tobs), cov(z, x)),
-                     cbind(cov(x, Tobs), cov(x)))
-  obs$s_xT_upper <- matrix(solve(invert_me)[-1,1], ncol(x), 1)
-
+  if(!is.null(controls)){
+    second_stage <- reformulate(c(T_name, controls), response = y_name)
+    first_stage <- reformulate(c(z_name, controls))
+    gamma_iv <- coefficients(AER::ivreg(second_stage, first_stage, data))[-c(1,2)]
+    # No intercept since we "project it out" by working with Cov matrix below
+    x <- model.matrix(reformulate(controls, intercept = FALSE), data)
+    Sigma <- rbind(cbind(cov(z, Tobs), cov(z, x)),
+                       cbind(cov(x, Tobs), cov(x)))
+    Sigma_inv <- solve(Sigma)
+    obs$s_zT_upper <- Sigma_inv[1,1]
+    s_xT_upper <- matrix(Sigma_inv[-1,1], ncol(x), 1)
+    obs$N1 <- drop(cov(z, x) %*% gamma_iv / var(z))
+    obs$N2 <- drop(cov(z, x) %*% s_xT_upper)
+    obs$N3 <- drop(cov(Tobs, x) %*% gamma_iv)
+    obs$N4 <- drop(cov(Tobs, x) %*% s_xT_upper)
+  }else{
+    # Case without covariates!
+    obs$N1 <- obs$N2 <- obs$N3 <- obs$N4 <- 0
+  }
   return(obs)
 }
 
-#AngristControls <- c("age", "svy", "sex2", "phone", "hsvisit",
-#                     "d1995", "djamundi", "dmonth1", "dmonth2",
-#                     "dmonth3", "dmonth4", "dmonth5", "dmonth6", "dmonth7",
-#                     "dmonth8", "dmonth9", "dmonth10", "dmonth11",
-#                     "strata1", "strata2", "strata3", "strata4", "strata5")
-#AngristY <- "totalRepeats"
-#AngristT <- "usesch"
-#AngristZ <- "vouch0"
-#
-#getObs(AngristY, AngristT, AngristZ, AngristControls, angrist)
+
