94d69ecbf2e065c330dd51ec86f4121c2ddc8170
Francis DiTraglia <fditra@sas.upenn.edu>
2016-02-25 17:32:00 -0500

    add documentation

---
+++ R/data-documentation.R
@@ -0,0 +1,39 @@
+#' Angrist et al. (2002) Dataset
+#'
+#' @description Description of the dataset goes here.
+#' @format A data frame with ??? rows and ??? variables:
+#' \describe{
+#'    \item{???}{???}
+#'    \item{???}{???}
+#'    \item{???}{???}
+#' }
+#' @source \url{http://economics.mit.edu/files/1395}
+#' @references \url{http://economics.mit.edu/faculty/angrist/data1/data/angetal02}
+"angrist"
+
+#' Dinkelman (2011) Dataset
+#'
+#' @description Description of the dataset goes here.
+#' @format A data frame with ??? rows and ??? variables:
+#' \describe{
+#'    \item{???}{???}
+#'    \item{???}{???}
+#'    \item{???}{???}
+#' }
+#' @source \url{https://www.aeaweb.org/aer/data/dec2011/20080791_data.zip}
+#' @references \url{http://www.jstor.org/stable/41408731}
+"dinkelman"
+
+#' Gelbach (2002) Dataset
+#'
+#' @description Description of the dataset goes here.
+#' @format A data frame with ??? rows and ??? variables:
+#' \describe{
+#'    \item{???}{???}
+#'    \item{???}{???}
+#'    \item{???}{???}
+#' }
+#' @source Provided by author.
+#' @references \url{http://www.jstor.org/stable/3083335}
+"gelbach"
+
