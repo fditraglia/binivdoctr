ff197e2a031d238266ec0b123a35ae16fa1f27b0
Francis DiTraglia <fditra@sas.upenn.edu>
2016-05-17 14:55:48 -0400

    finish simple implementation of Ishwaran and James and test: seems to work pretty well!

--- R/IshwaranJames.R
+++ R/IshwaranJames.R
@@ -2,10 +2,10 @@
 
 # Some fake data
 set.seed(2837)
-n <- 100
-x1 <- -1 + 0.5 * rnorm(n)
-x2 <- 1 + 1.5 * rnorm(n)
-y <- sample(0:1, n, replace = TRUE, prob = c(0.65, 0.35))
+nObs <- 100
+x1 <- -1 + rnorm(nObs)
+x2 <- 1 + rnorm(nObs)
+y <- sample(0:1, nObs, replace = TRUE, prob = c(0.65, 0.35))
 X <- y * x1 + (1 - y) * x2
 
 
@@ -16,74 +16,143 @@ nu1 <- nu2 <- 2
 eta1 <- eta2 <- 2
 
 # Maximum number of clusters
-M <- 50
+maxClust <- 50
 
 
-# initialize randomly with only 10 nonempty clusters
+# initialize randomly
 set.seed(2617)
-K <- factor(sample(c(1, 5, 7, 9, 13, 25, 36, 40), n, replace = TRUE), 1:M) #Keep track of empties!
+K <- factor(sample(1:50, nObs, replace = TRUE), 1:maxClust) #Keep track of empties!
 clusters <- tapply(X, K, c)
 empty <- sapply(clusters, is.null)
-n_empty <- sum(empty)
 mu <- tapply(X, K, mean)
 sigma_sq <- tapply(X, K, var)
-n_k <- tapply(X, K, length)
-n_k[is.na(n_k)] <- 0
-p <- (1 / n) * n_k
+# Handle clusters with only one observation
+sigma_sq[!empty & is.na(sigma_sq)] <- var(X)
+obsPerClust <- tapply(X, K, length)
+obsPerClust[is.na(obsPerClust)] <- 0
+p <- (1 / nObs) * obsPerClust
+theta <- mean(X)
+alpha <- rgamma(1, shape = eta1, rate = eta2)
+
+# object to store the draws
+nDraws <- 5000
+draws <- list(mu = matrix(NA_real_, nrow = maxClust, ncol = nDraws), 
+              sigma_sq = matrix(NA_real_, nrow = maxClust, ncol = nDraws), 
+              K = matrix(NA_integer_, nrow = nObs, ncol = nDraws), 
+              p = matrix(NA_real_, nrow = maxClust, ncol = nDraws), 
+              alpha = rep(NA_real_, nDraws), 
+              theta = rep(NA_real_, nDraws))
 
 
-# Another possible way?
-#clusters <- lapply(1:M, function(x) data[which(K == x)])
-#mu <- sapply(clusters, mean)
-#sigma_sq <- sapply(clusters, var)
-#p <- (1 / n) * sapply(clusters, length)
+# The Gibbs Sampler!
+for(i in 1:nDraws){
+  # update mu
+  s_sq <- rep(NA_real_, maxClust)
+  m <- rep(NA_real_, maxClust)
+  s_sq[!empty] <- 1 / (obsPerClust[!empty] / sigma_sq[!empty] + 1 / xi_sq)
+  m[!empty] <- s_sq[!empty] * (obsPerClust[!empty] * (theta / xi_sq) + 
+    sapply(clusters[!empty], sum) / sigma_sq[!empty])
+  m[empty] <- theta
+  s_sq[empty] <- xi_sq 
+  mu <- rnorm(maxClust, mean = m, sd = sqrt(s_sq))
+  
+  # update sigma_sq
+  rho <- rep(NA_real_, maxClust)
+  rho[!empty] <- mapply(function(y, z) sum((y - z)^2 / 2),  
+                        clusters[!empty], mu[!empty])
+  rho[empty] <- 0
+  sigma_sq <- 1 / rgamma(maxClust, shape = nu1 + obsPerClust / 2, 
+                         rate = nu2 + rho)
+  
+  # update cluster membership
+  pi <- matrix(rep(p / sqrt(sigma_sq), times = nObs) * 
+               exp(-0.5 * (rep(X, each = maxClust) - rep(mu, times = nObs))^2 / 
+               rep(sigma_sq, times = nObs)), nrow = maxClust, ncol = nObs, 
+               byrow = FALSE)
+  K <- factor(apply(pi, 2, function(x) sample(1:maxClust, size = 1, prob = x)), 
+              1:maxClust)
+  obsPerClust <- tapply(X, K, length)
+  obsPerClust[is.na(obsPerClust)] <- 0
+  clusters <- tapply(X, K, c)
+  empty <- sapply(clusters, is.null)
+  
+  
+  # update p
+  V <- rbeta(maxClust - 1, shape1 = 1 + obsPerClust[-maxClust], 
+             shape2 = alpha + cumsum(obsPerClust)[-1])
+  log_remain <- cumsum(log(1 - V))
+  p <- rep(NA_real_, maxClust)
+  p[-maxClust] <- c(V[1], exp(log(V)[-1] + log_remain[-(maxClust - 1)]))
+  p[maxClust] <- 1 - sum(p[-maxClust])
+  
+  # update alpha
+  alpha <- rgamma(1, shape = maxClust + eta1 - 1, 
+                  rate = eta2 - log_remain[(maxClust - 1)])
+  
+  # update theta
+  s_theta_sq <- 1 / (maxClust / xi_sq + 1 / A)
+  m_theta <- (s_theta_sq / xi_sq) * sum(mu)
+  theta <- rnorm(1, mean = m_theta, sd = sqrt(s_theta_sq))
+  
+  # store the draws
+  draws$mu[,i] <- mu
+  draws$sigma_sq[,i] <- sigma_sq
+  draws$K[,i] <- K
+  draws$p[,i] <- p
+  draws$alpha[i] <- alpha
+  draws$theta[i] <- theta
+  
+}
 
-# update mu
-s_sq <- rep(NA_real_, M)
-s_sq[!empty] <- 1 / (n_k[!empty] / sigma_sq[!empty] + 1 / xi_sq)
-#m[!empty] <- 
 
-m[empty] <- theta
-s_sq[empty] <- xi_sq 
-rnorm(M, mean = theta, sd = sqrt(xi_sq))
+plot(draws$alpha, type = 'l')
+plot(draws$theta, type = 'l')
+plot(apply(draws$K, 2, function(x) length(unique(x))), type = 'l')
 
+table(draws$K[,5000])
+round(draws$p[,5000], 4)
 
+plot_pdf <- function(n){
+  pdf <- function(x, n){
+    sum(draws$p[,n] * dnorm(x, mean = draws$mu[,n], sd = sqrt(draws$sigma_sq[,n])))
+  }
+  
+  phi <- Vectorize(function(x) pdf(x, n), "x")
+  curve(phi(x), from = -5, to = 5)
+}
 
-# update sigma_sq
+plot_pdf(sample(1000:5000, 1))
 
-# update cluster membership
-pi <- matrix(rep(p / sqrt(sigma_sq), times = n) * 
-             exp( -0.5 * (rep(X, each = M) - rep(mu, times = n))^2 / 
-             rep(sigma_sq, times = n)), nrow = M, ncol = n, byrow = FALSE)
-K <- factor(apply(pi, 2, function(x) sample(1:M, size = 1, prob = x)), 1:M)
-n_k <- tapply(X, K, length)
-n_k[is.na(n_k)] <- 0
+plot_cdf <- function(n){
+CDF <- function(x, n){
+  sum(draws$p[,n] * pnorm(x, mean = draws$mu[,n], sd = sqrt(draws$sigma_sq[,n])))
+}
 
-# update p
-V <- rbeta(M - 1, shape1 = 1 + n_k[-M], shape2 = cumsum(n_k)[-1])
-log_remain <- cumsum(log(1 - V))
-p <- c(V[1], exp(log(V)[-1] + log_remain[-(M - 1)]))
+Phi <- Vectorize(function(x) CDF(x, n), "x")
+curve(Phi(x), from = -5, to = 5, n = 1000)
+  
+}
 
-# update alpha
-alpha <- rgamma(1, shape = M + eta1 - 1, rate = eta2 - log_remain[(M - 1)])
+plot_cdf(sample(1000:5000, 1))
 
-# Update theta
-s_theta_sq <- 1 / (M / xi_sq + 1 / A)
-m_theta <- (s_theta_sq / xi_sq) * sum(mu)
-theta <- rnorm(1, mean = m_theta, sd = sqrt(s_theta_sq))
+# evaluate at posterior mean
+mu_pm <- apply(draws$mu[,-c(1:1000)], 1, mean)
+sigma_pm <- sqrt(apply(draws$sigma_sq[,-c(1:1000)], 1, mean))
+p_pm <- apply(draws$p[,-c(1:1000)], 1, mean)
 
+f_pm <- function(x){
+  sum(p_pm * dnorm(x, mean = mu_pm, sd = sigma_pm))
+}
 
+F_pm<- function(x){
+  sum(p_pm * pnorm(x, mean = mu_pm, sd = sigma_pm))
+}
 
+phi_pm <- Vectorize(f_pm, "x")
+Phi_pm<- Vectorize(F_pm, "x")
 
-
-
-
-
-
-
-
-
-
+curve(Phi_pm(x), from = -5, to = 5, n = 1000)
+curve(phi_pm(x), from = -5, to = 5, n = 1000)
 
 
 
